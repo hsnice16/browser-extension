1d0aa2365ce2260f81a1523c42aa445c89907568
Himanshu Singh <himanshusingh@Himanshus-MacBook-Air.local>
2023-04-08 13:00:01 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <div>Full View</div>
+      <h1>You're staring my extension</h1>
+      <a href="https://twitter.com/hsnice16">@hsnice16</a>
+    </div>
+  );
+}
+
+export default App;
